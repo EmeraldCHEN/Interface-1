b5a227314a9db1cbd4d90078810aeea5267c4d65
Your Name <you@example.com>
2019-03-12 10:29:35 +1300

    commit

---
+++ Interface 1/ITestInterface1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Interface_1
+{
+    // The default scope of the members of an interface is public
+    // whereas it's private in case of a class
+
+    // By default every member of an interface is abstract so we don't require to use abstract modifier
+    interface ITestInterface1
+    {
+        // int x; -> Interfaces cannot contain fields
+        void Add(int a, int b);
+    }
+    interface ITestInterface2: ITestInterface1
+    {
+        void Sub(int a, int b);
+    }
+    // If required an interface can inherit from aother interface
+    class ImplementationClass : ITestInterface2
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine("x + y = " + (x + y));
+        }
+        // Every member of an interface should be implemented under the child class
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine("x - y = " + (x - y));
+        } 
+    }
+}
---
+++ Interface 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interface_1
+{
+    // Class: user-defined data type, non-abstract methods(methods with method body)
+    class Program
+    {
+        static void Main()
+        {
+            ImplementationClass obj = new ImplementationClass();
+            obj.Add(100, 11);
+            obj.Sub(100, 11);
+
+            ITestInterface2 i = obj;
+            i.Add(100, 11);
+            i.Sub(100, 11);
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+/* Interface: user-defined data type only, contains only abstract method(methods without method body)
+              Every abstract method of an interface shoud be implemented by the child class of the interface
+
+   Abstract class: Non-abstract methods and also abstract methods
+
+    
+Note:
+    Generally a class inherits from another class to consume the members of its parent,
+
+    whereas if a class is inheriting from an interface it's to implement the the members of its parent
+                                                              
+    A class can inherit from a class and interface at a time
+    
+ */
